ab46d1029d42c6328adf3e57962ee1a3ad7c2954
arjun <arjun@arjuns-m3.local>
2025-11-09 01:49:14 +0530

    testing

--- dobble.py
+++ dobble.py
@@ -9,7 +9,7 @@ Segmentation net to divide into 2 cards, 8 symbols each.
 Run quick mapping to see if any symbol is in both cards (eg. if shape > 85% accurate)
 Pretrained areas for 50 different symbols
 TTS to say the actual symbol (random.randn time delay) - simulates an actual game
-
+asdf
 Resources:
 https://wandb.ai/ishandutta/semantic_segmentation_unet/reports/Semantic-Segmentation-with-UNets-in-PyTorch--VmlldzoyMzA3MTk1
 """
